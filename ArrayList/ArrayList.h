16aacb5ca2b238ee7ef4049f27982bb61aaa845a
Vladlite54 <255vlad25504@gmail.com>
2025-03-09 23:52:30 +0700

    Add consrtuctors and Node class

--- ArrayList/ArrayList.h
+++ ArrayList/ArrayList.h
@@ -2,20 +2,68 @@
 
 #include <iostream>
 
-
 template<typename T>
 class ArrayList {
 public:
-/*
-TO DO:
-    methods
-    constructors
-*/
+    ArrayList(int capacity);    // конструктор списка
+    ArrayList(const ArrayList<T> &other);  // конструктор копирования
+    ~ArrayList();   // деструктор
+    int size(); // опрос размера списка
+    void clear();   // очистка списка
+    bool empty();   // проверка списка на пустоту
+
 
 private:
-/*
-TO DO:
-    data
-    maybe node class
-*/
+    class Node {    // узел списка
+    public:
+        T value;    // значение в узле
+        int next;   // индекс следующего узла
+        int prev;   // индекс предыдущего узла
+        Node(T value);  // конструктор узла
+    };
+
+    Node* array;    // массив, хранящий узлы
+    int head;   // индекс первого элемента списка
+    int tail;   // индекс последнего элемента списка
+    int firstFree;  // индекс первого свободного элемента в списке
+    int capacity;   // емкость списка  
+    int size;   // размер списка
+
+    void resize();  // увеличить емкость списка
+
 };
+
+///////////// ПУБЛИЧНЫЕ МЕТОДЫ /////////////
+
+// Конструктор списка
+template <typename T>
+ArrayList<T>::ArrayList(int capacity) {
+    this->capacity = capacity;
+    size = 0;
+    head = tail = -1;
+    array = new Node[this->capacity];
+}
+
+///////////// ПРИВАТНЫЕ МЕТОДЫ /////////////
+
+template <typename T>
+void ArrayList<T>::resize() {
+    Node* newArr = new Node[capacity * 2];
+    for (int i = 0; i < capacity; i++) {
+        newArr[i] = array[i];
+    }
+    delete[] array;
+    array = newArr;
+    capacity = capacity * 2;
+}
+
+
+// Конструктор узла списка
+template <typename T>
+ArrayList<T>::Node::Node(T value) {
+    this->value = value;
+    this->next = -1;
+    this->prev = -1;
+}
+
+
