74abefc469b6e4c98429eaeff4da7a5dd887be4b
Vladlite54 <255vlad25504@gmail.com>
2025-03-10 01:46:11 +0700

    Add destructor, copy constructor, getSize method, empty method

--- ArrayList/ArrayList.h
+++ ArrayList/ArrayList.h
@@ -5,21 +5,29 @@
 template<typename T>
 class ArrayList {
 public:
-    ArrayList(int capacity);    // конструктор списка
+    ArrayList(int capacity = 16);    // конструктор списка
     ArrayList(const ArrayList<T> &other);  // конструктор копирования
     ~ArrayList();   // деструктор
-    int size(); // опрос размера списка
+    int getSize(); // опрос размера списка
     void clear();   // очистка списка
     bool empty();   // проверка списка на пустоту
 
+    class iterator {
+
+    };
+
 
 private:
-    class Node {    // узел списка
-    public:
+    struct Node {    // узел списка
         T value;    // значение в узле
         int next;   // индекс следующего узла
         int prev;   // индекс предыдущего узла
-        Node(T value);  // конструктор узла
+        Node() : Node(T(), -1, -1) {};  // конструктор по умолчанию узла
+        Node(T value, int next, int prev) : next{-1}, prev{-1} { // конструктор узла
+            this->value = value;
+            this->next = next;
+            this->prev = prev;
+        }
     };
 
     Node* array;    // массив, хранящий узлы
@@ -41,7 +49,36 @@ ArrayList<T>::ArrayList(int capacity) {
     this->capacity = capacity;
     size = 0;
     head = tail = -1;
-    array = new Node[this->capacity];
+    firstFree = 0;
+    this->array = new Node[this->capacity];
+}
+
+// Конструктор копирования
+template <typename T>
+ArrayList<T>::ArrayList(const ArrayList<T> &other) : head{other.head}, tail{other.tail},
+ firstFree{other.firstFree}, capacity{other.capacity}, size{other.size}  {
+    this->array = new Node[capacity];
+    for (int i = 0; i < capacity; i++) {
+        this->array[i] = other.array[i];
+    }
+}
+
+// Деструктор списка
+template <typename T>
+ArrayList<T>::~ArrayList()  {
+    delete[] array;
+}
+
+// Опрос размера списка
+template <typename T>
+int ArrayList<T>::getSize() {    
+    return size;
+}
+
+// Проверка списка на пустоту
+template <typename T>
+bool ArrayList<T>::empty() {
+    return size == 0;
 }
 
 ///////////// ПРИВАТНЫЕ МЕТОДЫ /////////////
@@ -56,14 +93,4 @@ void ArrayList<T>::resize() {
     array = newArr;
     capacity = capacity * 2;
 }
-
-
-// Конструктор узла списка
-template <typename T>
-ArrayList<T>::Node::Node(T value) {
-    this->value = value;
-    this->next = -1;
-    this->prev = -1;
-}
-
 
--- ArrayList/main.cpp
+++ ArrayList/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "ArrayList.h"
 
+
 int main() {
+
+    ArrayList<int> a(4);
 
     return 0;
 }
