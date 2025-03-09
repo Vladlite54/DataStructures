cf3364a5727958519f4d079926eab0b7148f2354
Vladlite54 <255vlad25504@gmail.com>
2025-03-09 19:00:55 +0700

    Add base structure

---
+++ ArrayList/ArrayList.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+
+template<typename T>
+class ArrayList {
+public:
+/*
+TO DO:
+    methods
+    constructors
+*/
+
+private:
+/*
+TO DO:
+    data
+    maybe node class
+*/
+};
